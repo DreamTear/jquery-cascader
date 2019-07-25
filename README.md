9bad44df9cbd2d7780d327805fa607629f38e471
DreamTear <DreamTear@users.noreply.github.com>
2019-07-25 16:29:42 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,79 @@
+# 级联选择
+这是一个基于jquery的级联选择插件，插件中使用的bootstrap的样式
+![演示图](/img/截图20190725143553.png)
+
+# 使用方法
+```html
+<input id="domId" value="2-1"/>
+```
+```javascript
+var data = [
+	{
+		id: '1',
+		name: 'text1',
+		children: [
+			{name: 'text1-1', id: '1-1'},
+			{name: 'text1-2', id: '1-2'},
+			{name: 'text1-3', id: '1-3'}
+		]
+	},
+	{
+		id: '2',
+		name: 'text2',
+		children: [
+			{name: 'text2-1', id: '2-1'},
+			{name: 'text2-2', id: '2-2'},
+			{name: 'text2-3', id: '2-3'}
+		]
+	},
+	{
+		id: '3',
+		name: 'text3',
+		children: [
+			{name: 'text3-1', id: '3-1'},
+			{name: 'text3-2', id: '3-2'},
+			{name: 'text3-3', id: '3-3'}
+		]
+	},
+];
+$('#domId').cascader({
+	displayField: 'name',
+	valueField: 'id',
+	separator: '/',
+	data: data
+});
+```
+级联选择在初始化的时候回去读取input的value进行初始化
+
+# 配置项
+
+## displayField 
+
+配置显示的字段名称，默认值为'name'
+
+## valueField 
+
+配置提交的字段名称，默认值为'id'
+
+## separator 
+
+配置显示字段的连接符，默认值为'/'
+
+## data
+
+级联菜单的数据，含有子项的需要有chidlren字段, 默认值为[]
+
+# API
+
+## clearValue
+
+清空当前选择的内容
+
+# EVENT
+
+## change.cascader
+参数：
+	value 提交的值
+	checkeditems 选中的节点到一级节点的数组
+
+当选中内容发生改变时触发，如果某一项有子级，点击不会触发。
---
+++ src/jquery.cascader.js
@@ -0,0 +1,247 @@
+/**
+ * jquery.cascader.js 级联菜单
+ * @authors wangy (wangy@huilan.com)
+ * @date    2019-07-19 17:03:00
+ * @version $Id$
+ */
+
++function($) {
+	function Cascader (opt) {
+		var def = {
+			displayField: 'name',
+			valueField: 'id',
+			separator: '/',
+			data: []
+		};
+		this.el = opt.el;
+		this.$el = $(this.el);
+		this.data = opt.data;
+		this.option = $.extend(def, opt);
+		this.checkeditems = [];
+		this.inited = false;
+
+		this.init();
+	}
+	Cascader.prototype.init = function() {
+		this.wrapEl();
+		this.creatDropdownEl();
+		// var $menu = this.creatCascaderMenu(this.data);
+		// this.$dropdownEl.append($menu);
+		this.initValue();
+		this.inited = true;
+	};
+	Cascader.prototype.wrapEl = function() {
+		var me = this;
+		var $showEl = $('<div class="input-group">'
+		  + '<input type="text" class="form-control"/>'
+		  + '<span class="input-group-addon"><i class="glyphicon glyphicon-chevron-down"></i></span>'
+		+ '</div>');
+		// var $warperEl = $('<div class="cascader-wrapper"></div>');
+		me.$el.wrap('<div class="cascader-wrapper"></div>').hide();
+		me.$el.after($showEl);
+		me.$showInput = $('input', $showEl);
+		me.$triggerEl = $('.input-group-addon', $showEl);
+
+	}
+	Cascader.prototype.creatDropdownEl = function() {
+		var position = this.$showInput.offset();
+		var elH = this.$showInput.outerHeight(true); // 计算input带边框的高度
+		this.$dropdownEl = $('<div class="cascader-dropdown-container"></div>');
+		this.$dropdownEl.css({
+			left: position.left,
+			top: position.top + elH
+		});
+		$('body').append(this.$dropdownEl);
+		this.initEvent();
+	};
+	Cascader.prototype.creatCascaderMenu = function(data, selectItem) {
+		var $menu = $('<ul class="cascader-menu"></ul>')
+		$.each(data, function(index, item) {
+			var $item = $('<li class="cascader-menu-item">' + item.name + '</li>');
+			if (item.children) {
+				$item.addClass('cascader-menu-item-expand')
+				$item.append('<i class="glyphicon glyphicon-menu-right"></i>')
+			}
+			if(selectItem && item == selectItem) {
+				$item.addClass('cascader-menu-item-active');
+			}
+			$item.data('cascader-data', item)
+			$menu.append($item)
+		})
+		return $menu;
+	};
+	Cascader.prototype.initEvent = function() {
+		var me = this;
+		var timer;
+		me.$dropdownEl.on('click', 'li', function(event) {
+			var $this = $(this);
+			var data = $this.data('cascader-data');
+			var $parent = $this.parent();
+			var parentIndex = $parent.index();
+			var checkeditems = me.checkeditems;
+
+			if (timer) {
+				clearTimeout(timer);
+			}
+
+			if ($this.hasClass('cascader-menu-item-active')) {
+				if(data.children) {
+					return
+				} else {
+					me.hideDropdown();
+				}
+			}
+
+			$this.addClass('cascader-menu-item-active').siblings().removeClass('cascader-menu-item-active');
+			$parent.nextAll().remove();
+
+			if(checkeditems.length > parentIndex) {
+				checkeditems = checkeditems.slice(0, parentIndex);
+			}
+			checkeditems.push(data);
+
+			if (data.children) {
+				var $nextMenu = me.creatCascaderMenu(data.children);
+				$parent.after($nextMenu);
+			}else {
+				me.checkeditems = checkeditems;
+				me.setValue();
+				me.hideDropdown();
+			}
+		});
+		me.$showInput.on('focus', function(event) {
+			event.stopPropagation();
+			me.showDropdown();
+		});
+		// me.$showInput.on('blur', function(event) {
+		// 	event.stopPropagation();
+		// 	me.hideDropdown();
+		// });
+		me.$showInput.on('click', function(event) {
+			event.stopPropagation();
+		});
+		me.$triggerEl.on('click', function(event) {
+			event.stopPropagation();
+			if(me.$dropdownEl.is(':visible')) {
+				me.hideDropdown();
+			} else {
+				me.showDropdown();
+			}
+		});
+		$(document).on('click', function(event) {
+			var target = event.target;
+			if ($(target).hasClass('cascader-menu-item')) {
+				return;
+			}
+			timer = setTimeout(function() {
+				me.hideDropdown();
+			}, 2);
+		})
+
+	};
+	Cascader.prototype.showDropdown = function() {
+		this.$dropdownEl.show();
+	};
+	Cascader.prototype.hideDropdown = function() {
+		this.$dropdownEl.hide();
+	};
+	Cascader.prototype.setValue = function() {
+		var me = this;
+		var option = me.option;
+		var showProp = option.displayField;
+		var submitProp = option.valueField;
+		var showValue = [];
+		var len = me.checkeditems.length;
+		var submitValue =  len > 0 ? me.checkeditems[len - 1][submitProp] : '';
+		$.each(me.checkeditems, function(index, item) {
+			showValue.push(item[showProp]);
+		});
+		me.$showInput.val(showValue.join(option.separator));
+		// 不是初始化完成前调用setValue，不触发事件；
+		if (!me.inited || me.oldSubmitValue == submitValue) {
+			return;
+		}
+		me.oldSubmitValue = submitValue;
+		me.$el.val(submitValue);
+		me.$el.trigger('change.cascader', [submitValue, me.checkeditems]);
+	};
+	Cascader.prototype.clearValue = function() {
+		var me = this;
+		me.checkeditems = [];
+		me.setValue();
+	};
+	Cascader.prototype.initValue = function() {
+		var me = this;
+		var val = me.$el.val().trim();
+		// if(!val) {
+		// 	return
+		// }
+
+		var path = getPath(me.data, val, me.option.valueField);
+		me.oldSubmitValue = val;
+		me.checkeditems = path;
+		me.initMenu(me.checkeditems);
+		me.setValue();
+	};
+	Cascader.prototype.initMenu = function() {
+		var me = this;
+		var data = me.data;
+		var $menu
+
+		if (me.checkeditems.length === 0) {
+			$menu = me.creatCascaderMenu(me.data);
+			me.$dropdownEl.append($menu);
+		}
+		
+		$.each(me.checkeditems, function(index, item) {
+			$menu = me.creatCascaderMenu(data, item);
+			me.$dropdownEl.append($menu);
+			if(item.children) {
+				data = item.children;
+			}
+			
+		})
+	};
+
+	function getPath(data, val, key) {
+		var obj = [];
+		for(var i = 0,l = data.length; i< l; i ++) {
+			var item = data[i];
+			if(item[key] == val) {
+				obj.push(item);
+				break;
+			} else if(item.children) {
+				var subPath = getPath(item.children, val, key);
+				if (subPath.length > 0) {
+					obj = obj.concat(item, subPath);
+				}
+			}
+		}
+		return obj;
+	}
+
+
+$.fn.cascader = function(opt) {
+	var res;
+	this.each(function() {
+		var $this = $(this);
+		var cascader = $this.data('cascader');
+		if(cascader) {
+			res = cascader[opt]();
+			if(res !== undefined) {
+				return false
+			}
+		} else {
+			var option = $.extend({el: this}, opt);
+			$this.data('cascader', new Cascader(option));
+		}
+		
+	});
+	if(res !== undefined) {
+		return res;
+	} else {
+		return this;
+	}
+};
+
+}(jQuery);
